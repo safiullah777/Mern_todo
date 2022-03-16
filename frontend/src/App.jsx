b7b3e8e0c4dbc65e7845fa05978c2f3f5a8cc138
safiullah777 <57296932+safiullah777@users.noreply.github.com>
2022-03-16 21:13:11 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,33 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+
 import { useAuth0 } from "@auth0/auth0-react";
-import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
-// import {} from 'reac'
-import axios from "axios";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import FrontPage from "./Pages/FrontPage/index";
 import Authenticate from "./Pages/authentication";
-import { PrivateRoute } from "./components/PrivateRoute";
+import RequireAuth from "./AuthProvider/requireAuth";
 import Loader from "./components/loader";
 
-
-
 function App() {
-  const {isAuthenticated,loginWithRedirect,user,isLoading} = useAuth0();
+  const { isLoading } = useAuth0();
 
   return (
     <>
-     {/* {!isLoading ? <BrowserRouter>
-        <Routes>
-        {isAuthenticated ?<Route element={<PrivateRoute />} >
-            <Route path="/"  element={<FrontPage />} />
-         </Route>:
-        <Route path='/authenticate'  element={<Authenticate />} /> }  
-        <Route path="*" element={<Navigate to="/" replace />} />    
-        </Routes> 
-      </BrowserRouter>
-      :<Loader />} */}
-      {!isLoading && isAuthenticated? <FrontPage />:!isLoading && !isAuthenticated ?<Authenticate />:<Loader />}
+      {!isLoading ? (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/authenticate" element={<Authenticate />} />
+            <Route
+              path="/"
+              element={
+                <RequireAuth redirectTo="/authenticate">
+                  <FrontPage />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 }
